6acd1a14c30acb72851ea7c44a96748400839e02
saad <saadnh05@gmail.com>
2025-01-26 01:27:24 +0100

    Initialize React app structure; remove unused files and configure Tailwind CSS

---
+++ client/src/constants/index.js
@@ -0,0 +1,228 @@
+// Desc: Constants for the website
+// Date: 9th April 2021
+// Version: 1.0.0
+// Website Constants
+export const websiteConstants = {
+    // Website URL
+    WEBSITE_URL: 'https://PrepGenius.com',
+    // Website Name
+    WEBSITE_NAME: 'PrepGenius',
+    // Website Description
+    WEBSITE_DESCRIPTION: 'Create a Quiz instantly, with your own files!',
+    // Website Keywords
+    WEBSITE_KEYWORDS: 'quiz, exam, test, multiple choice, true or false, short answer, generate, create, document, pdf, word file, text file, image, presentation',
+    // Website Author
+    WEBSITE_AUTHOR: 'Mourabit Saad',
+};
+// Landing page Constants
+// Navigation Links
+export const navlinks = [
+    {
+        title: 'How it works',
+        url: '/how-it-works'
+    },
+    {
+        title: 'Features',
+        url: '/features'
+    },
+    {
+        title: 'Pricing',
+        url: '/pricing'
+    },
+    {
+        title: 'About', 
+        url: '/about'
+    },
+]
+// Hero Text
+export const herotext = {
+    title: 'Create a Quiz instantly, with your own files!',
+    subtitle: "Unlock your potential with our innovative exam maker. Whether you're a student gearing up for exams or a teacher designing assessments, our tool is your go-to solution. Effortlessly create professional multiple-choice, true or false, and short-answer exams in any subject and language.",
+    upsub: 'Available in all the  Languages',
+    cta: 'Create Your First Exam ',
+    url: '/login',
+    lmbtn: 'Learn More',
+    url2: '/learn-more',
+    heroimage: 'https://via.placeholder.com/500'
+}
+export const herotext2 = {
+    title: 'The Ultimate Quiz Generator From Any {variable}',
+    variable: ['Document', 'PDF', 'Word File', 'Text File', 'Image', 'Presentation'],
+    subtitle: "Create quizzes from any document, including PDFs, Word files, and more. Our tool automatically extracts text from your files and generates questions based on the content. Say goodbye to manual question creation and hello to instant quizzes!",
+    cta: 'Create Your First Exam ',
+    url: '/login',
+}
+export const Usedby = {
+    title: 'Educators, Students, and Professionals around the world',
+    images: [
+        'https://via.placeholder.com/150',
+        'https://via.placeholder.com/150',
+        'https://via.placeholder.com/150',
+        'https://via.placeholder.com/150',
+    ]
+}
+export const howitworks = [
+    {
+        title: 'How it works',
+        Steps: ['Step 1: Upload your file', 'Step 2: Generate your quiz', 'Step 3: Test Your Knowledge', 'Step 4: Share your quiz'],
+    },
+    {
+        title: 'Sign in and learn faster.',
+        subtitle: ' Our platform is designed to help you learn more efficiently and effectively. Sign up today and start creating quizzes in minutes!',
+        cta: 'Go study',
+    }
+]
+export const features = [
+    {
+        title: 'Create quizzes from any document',
+        subtitle: 'Upload any document, including PDFs, Word files, and more, and our tool will automatically extract text and generate questions based on the content.',
+        image: 'https://via.placeholder.com/150',
+        cta: 'Try it now',
+        Link: '/login'
+    },
+    {
+        title: 'Instant quizzes',
+        subtitle: 'Effortlessly create professional multiple-choice, true or false, and short-answer exams in any subject and language.',
+        image: 'https://via.placeholder.com/150',
+        cta: 'Try it now',
+        Link: '/login'
+    },
+    {
+        title: 'Collaborate with others',
+        subtitle: 'Invite your friends, classmates, or colleagues to collaborate on quizzes and share knowledge.',
+        image: 'https://via.placeholder.com/150',
+        cta: 'Try it now',
+        Link: '/login'
+    },
+]
+export const pricing = [
+    {
+        title: 'Free',
+        subtitle: 'Get started with 5 exam generations.',
+        price: '$0',
+        features: ['Create 5 Exam Generations','Share quizzes', 'Access to All Features','No Ads', 'No Limit on Questions'],
+        cta: 'Checkout',
+    },
+    {
+        title: 'Standard',
+        subtitle: 'Get started with 20 exam generations.',
+        old_price: '$9.99',
+        price: '$4.99',
+        features: ['Create 20 Exam Generations','Share quizzes', 'Access to All Features','No Ads', 'No Limit on Questions'],
+    },
+    {
+        title: 'Lifetime',
+        subtitle: 'Unlimited exam generations for life.',
+        old_price: '$25.99',
+        price: '$19.99',
+        features: ['Create Unlimited Exam Generations','Share quizzes', 'Access to All Features','No Ads', 'No Limit on Questions', 'Free Updates Forever'],
+    }
+]
+export const whyus = [
+    {
+        title: 'Instant Exam Generation',
+        subtitle: "Whether you're a student gearing up for exams or a teacher designing assessments, our Multiple Choice Exam Maker is your go-to solution. Instantly create professional, multiple choice tests in any subject and language with ease",
+    },
+    {
+        title: 'Collaboration Made Easy',
+        subtitle: "Invite your friends, classmates, or colleagues to collaborate on quizzes and share knowledge. Our platform is designed to help you learn more efficiently and effectively.",
+    },
+    {
+        title: 'Accurate Questions',
+        subtitle: "Our  Exam Maker crafts relevant, challenging questions that truly test your understanding of the material.",
+    },
+    {
+        title: 'Flexible Input Options',
+        subtitle: "Create multiple-choice, true or false, and short-answer exams from text, PDFs, and images. Our Exam Maker effortlessly handles all formats.",
+    },
+    {
+        title: 'Save Time',
+        subtitle: "Stop spending heures creating multiple choice tests manually. Our exam maker does the work for you in seconds.",
+    },
+    {
+        title: 'Study Smarter',
+        subtitle: "Practice with auto-generated multiple-choice, true or false, and short-answer questions to boost retention and test readiness.",
+    }
+]
+export const about = [
+    {
+        title: 'Our Mission',
+        subtitle: 'Our mission is to help students, teachers, and professionals around the world learn more efficiently and effectively. Our innovative exam maker is designed to make studying easier and more enjoyable.',
+    },
+    {
+        title: 'Our Team',
+        subtitle: 'Our team is made up of passionate educators, developers, and designers who are dedicated to creating the best possible learning experience for our users. We are constantly working to improve our platform and add new features.',
+    },
+    {
+        title: 'Our Story',
+        subtitle: 'Our story began with a simple idea: to make studying easier and more enjoyable. We started with a small team of developers and designers and have since grown into a global company with millions of users worldwide.',
+    },
+    {
+        title: 'Our Values',
+        subtitle: 'We value innovation, collaboration, and customer satisfaction above all else. We are committed to creating the best possible learning experience for our users and are always looking for ways to improve our platform.',
+    }
+]
+export const faqs = [
+    {
+        question: 'What is PrepGenius ?',
+        answer: 'PrepGenius is a tool that allows you to create multiple-choice, true or false, and short-answer exams in any subject and language. You can upload any document, including PDFs, Word files, and more, and our tool will automatically extract text and generate questions based on the content.',
+    },
+    {
+        question: 'How do I use PrepGenius ?',
+        answer: 'To use PrepGenius, simply upload your document, select the type of questions you want to generate, and click "Create Quiz." Our tool will automatically extract text from your document and generate questions based on the content. You can then edit the questions as needed and share your quiz with others.',
+    },
+    {
+        question: 'Is PrepGenius suitable for all types of exams?',
+        answer: 'Yes, PrepGenius is suitable for all types of exams, including multiple-choice, true or false, and short-answer tests. You can create quizzes in any subject and language and customize the questions to suit your needs.',
+    },
+    {
+        question: 'Can I save or share the generated tests?',
+        answer: 'Yes, you can save and share the generated tests with others. You can download the quizzes as PDFs or share them via email, social media, or other platforms. You can also collaborate with others on quizzes and share knowledge.',
+    },
+    {
+        question: 'Is PrepGenius free?',
+        answer: 'PrepGenius offers a free trial, and then it is a subscription-based service. You can choose from a free plan, a standard plan, or a lifetime plan, depending on your needs. Each plan offers different features and benefits.',
+    }
+]
+// contact us  Constants
+export const contact = {
+    title: 'Contact Us',
+    subtitle: 'Have a question or feedback? We would love to hear from you! Fill out the form below, and we will get back to you as soon as possible.',
+    cta: 'Contact Us',
+    url: '/contact',
+}
+// Footer Constants
+export const footerlinks = [
+    {
+        title: 'About',
+        links: ['Our Mission', 'Our Team', 'Our Story', 'Our Values'],
+
+    },
+    {
+        title: 'Resources',
+        links: ['How it works', 'Features', 'Pricing', 'FAQs', 'Contact'],
+    },
+    {
+        title: 'Social',
+        links: [
+            {
+                title: 'Facebook',
+                url: 'https://facebook.com'
+            },
+            {
+                title: 'Twitter',
+                url: 'https://twitter.com'
+            },
+            {
+                title: 'Instagram',
+                url: 'https://instagram.com'
+            },
+            {
+                title: 'LinkedIn',
+                url: 'https://linkedin.com'
+            },
+        ],
+    }
+]
+// End Landing page Constants
+
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,34 @@
-// tailwind.config.js
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Assurez-vous que vos fichiers sont inclus ici
-    "./public/index.html",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        sans:['Roboto','sans-serif'],
+        poppins:['Poppins','sans-serif'],
+      },
+      colors: {
+        primary: '#3498db',
+        secondary: '#f1c40f',
+        dark: '#2c3e50',
+        light: '#ecf0f1',
+        error: '#e74c3c',
+        success: '#2ecc71',
+        warning: '#f1c40f',
+        info: '#3498db',
+        },
+        screens: {
+          xs: "480px",
+          ss: "620px",
+          sm: '768px',
+          md: '1060px',
+          lg: '1200px',
+          xl: '1700px',
+        },
+    },
   },
   plugins: [],
-};
+}
