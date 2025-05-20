874d7971b10c9e357ea80a2631cedc6195ca6dd5
AdnaneEchh <adnaneechhibou2005@gmail.com>
2025-05-20 22:22:22 +0100

    Add username parameter to uploadLesson function and update ProfUpload component to retrieve username

--- client/src/services/StudentService.jsx
+++ client/src/services/StudentService.jsx
@@ -3,16 +3,17 @@ import axios from "axios";
 const token = localStorage.getItem("access_token") || null;
 // uploade file or image or text to the server and create a lesson in the database , 
 // return the lesson objectID
-export const uploadLesson = async (data, title, type) => {
+export const uploadLesson = async (data, title, type, username) => {
   const formData = new FormData();
   // Add common fields
-  formData.append('title', title);  
+  formData.append('title', title);
+  formData.append('username', username);
   // Add content based on type
   if (type === 'file') {
-    formData.append('file', data, data.name); 
+    formData.append('file', data, data.name);
   } 
   else if (type === 'text') {
-    formData.append('text', data); 
+    formData.append('text', data);
   }
 
   // Log form data for debugging
