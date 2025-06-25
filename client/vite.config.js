9c69cbd1b01ba42ae764d819fc3305883a1aadff
saadmbt <saadnh05@gmail.com>
2025-06-25 16:21:23 +0100

    update Vite configuration to include build options for minification and output directory

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "/",
   plugins: [react()],
+  build: {
+    minify: "esbuild",
+    outDir: "dist",
+  },
   server: {
     proxy: {
       '/api': {
