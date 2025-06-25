52990b2b6f5bdfa0cdedcd6b5cade22f473354ae
saadmbt <saadnh05@gmail.com>
2025-06-25 23:29:51 +0100

    update import path for Navbar to match file naming convention

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom'
-import Navbar from '../components/landingpage/Navbar';
+import Navbar from '../components/landingpage/navbar';
 import Footer from '../components/landingpage/Footer';
 
 
